b4908b8606f9282f2b8e168548c47e31f0c33be2
Christian Chukwu <christianchukwu@Christians-MacBook-Pro-2.local>
2024-01-11 20:22:52 -0700

    created terraform modular directory structure

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "digitalocean" {
+  token = "${var.DIGITALOCEAN_TOKEN}"
+}
+
+# ... (rest of your Terraform configuration) ...
